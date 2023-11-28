14f01c8f0ff7add846795894d4c3563075d3814e
Gaurav Tembhare <90921095+gauravtembhare@users.noreply.github.com>
2023-11-28 11:01:11 +0000

    comparision done

---
+++ Basics/comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//don't do this below comparision
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
